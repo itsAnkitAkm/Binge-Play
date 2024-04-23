19fdcbb2aa6d798e0973ea0810d097b32b8ca986
Ankit Kumar Mishra <itsamkankit@gmail.com>
2024-04-23 18:00:49 +0530

    add bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,7 +165,7 @@ const logoutUser = asyncHandler(async(req, res)=>{
     req.user._id,
     {
       $set: {
-        refreshToken: undefined,
+        refreshToken: 1,
       },
     },
   { new: true }
